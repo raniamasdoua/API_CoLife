b326f3dd181d86eb9ef5b39c7aee3e9fa6007492
raniamasdoua <masdouarania02@gmail.com>
2026-03-23 22:24:29 +0100

    recuperer les activités

--- api/src/main/java/com/example/api/activityType/presentation/ActivityTypeController.java
+++ api/src/main/java/com/example/api/activityType/presentation/ActivityTypeController.java
@@ -1,4 +1,26 @@
 package com.example.api.activityType.presentation;
 
+import com.example.api.activityType.application.ActivityTypeUseCase;
+import com.example.api.activityType.application.dto.ActivityTypeResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/activity-types")
 public class ActivityTypeController {
+
+    private final ActivityTypeUseCase activityTypeUseCase;
+
+    public ActivityTypeController(ActivityTypeUseCase activityTypeUseCase) {
+        this.activityTypeUseCase = activityTypeUseCase;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityTypeResponseDto>> findAll() {
+        return ResponseEntity.ok(activityTypeUseCase.findAll());
+    }
 }
